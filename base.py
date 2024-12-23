fee0f14c84750d8b640d36d5d35dc5631c000c57
Tyler Algigi <44593496+tyleralgigi@users.noreply.github.com>
2024-12-23 16:53:32 -0500

    Base Code Structure
    Creating base py file and first py worker.
    First working gets Breakingpoint data

---
+++ base.py
@@ -0,0 +1,9 @@
+import worker
+
+def main():
+    worker.CDL_Worker().init()
+
+if __name__=="__main__":
+    main()
+
+
---
+++ worker.py
@@ -0,0 +1,101 @@
+import json
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
+import pandas as pd
+import requests
+from datetime import datetime
+
+class CDL_Worker:
+    def dbconnector(self):
+        print("Connecting to DB")
+        load_dotenv() # you can specify a location to your .env file as an argument if it's not at your project root
+        self.POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+        self.engine = create_engine(f'postgresql://{"postgres"}:{self.POSTGRES_PASSWORD}@localhost:{"5432"}/{"Data"}')
+
+        # self.conn = psycopg2.connect(
+        #     dbname="Data",
+        #     user="postgres",
+        #     password=self.POSTGRES_PASSWORD,
+        #     host="localhost",
+        #     port="5432"
+        # )
+
+    def iterator(self):
+        print("Iterating")
+        with open('config.json', 'r') as f:
+            config = json.load(f)
+            self.endpoints = config['API']['endpoints']
+
+                    
+        for endpoint in self.endpoints:
+            print(config[endpoint]['url'])
+            for url in config[endpoint]['url']:
+                self.url = url
+                self.tableName = config[endpoint]['table_name']
+                
+                eval(config[endpoint]['method'])
+
+    def breakpoint_players(self):
+        print("Running breakpoint_players")
+        response = requests.post(self.url)
+        if response.status_code == 200:
+            self.df = pd.DataFrame(response.json())
+            # print(self.df)
+            self.transform()
+            self.loader('replace')
+        else:
+            print(f"Failed to retrieve webpage: {response.status_code}")
+    
+    def breakpoint_matches(self):
+        print("Running breakpoint_matches")
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            response_df = response.json()
+            # Get the keys
+            keys = response_df['pageProps'].keys()
+            base_table_name = self.tableName
+            for key in keys:
+                if key not in ['initialColorScheme','event','matches_eventPlacements','mainEventId']:
+                    self.tableName = base_table_name + "_" + key
+                    try:
+                        self.df = pd.DataFrame(response_df['pageProps'][key]).astype(str)
+                        # print(self.df)
+                        self.check_ids()
+                        if len(self.df) > 0:
+                            self.transform()
+                            self.loader('append')
+                    except Exception as e:
+                        print("Unable to make data frame for " + key + " : "+ str(e))
+                    
+        else:
+            print(f"Failed to retrieve webpage: {response.status_code}")
+
+    def check_ids(self):
+        try:
+            query = f'select id from public."{self.tableName}"'
+            print(query)
+            df = pd.read_sql_query(query, self.engine)
+            if not df.empty:
+                self.df = self.df[~self.df['id'].isin(df['id'])]
+                print("compared and len is: " + str(len(self.df)))
+        except Exception as e:
+            print("SQL error: "+ str(e))
+
+        
+
+    def transform(self):
+        print("Transforming - Adding rundate")
+        now = datetime.now()
+        self.df['rundate'] = now.strftime("%Y%m%d")
+
+    def loader(self,method):
+        print("Loading into DB")
+        # Insert the DataFrame into a table
+        self.df.to_sql(self.tableName, self.engine, if_exists=method, index=False)
+
+    def init(self):
+        self.dbconnector()
+        self.iterator()
+        
+        
