0a781c332267a69ef9310525201b92ddc16ee24e
Tyler Algigi <44593496+tyleralgigi@users.noreply.github.com>
2024-12-24 21:40:43 -0500

    Completed Worker
    Matches, basic player stats and advanced details

--- worker.py
+++ worker.py
@@ -13,20 +13,12 @@ class CDL_Worker:
         self.POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
         self.engine = create_engine(f'postgresql://{"postgres"}:{self.POSTGRES_PASSWORD}@localhost:{"5432"}/{"Data"}')
 
-        # self.conn = psycopg2.connect(
-        #     dbname="Data",
-        #     user="postgres",
-        #     password=self.POSTGRES_PASSWORD,
-        #     host="localhost",
-        #     port="5432"
-        # )
-
     def iterator(self):
         print("Iterating")
         with open('config.json', 'r') as f:
             config = json.load(f)
             self.endpoints = config['API']['endpoints']
-
+            self.apikey = config['API']['apikey'] #this is the key used in breakpoint api calls (is subject to be removed at any point)
                     
         for endpoint in self.endpoints:
             print(config[endpoint]['url'])
@@ -47,6 +39,34 @@ class CDL_Worker:
         else:
             print(f"Failed to retrieve webpage: {response.status_code}")
     
+    def playerStatsDetials(self):
+        print("playerStatsDetials")
+        #get matches id, complete date
+        with self.engine.connect() as conn:
+                print("Getting match dates and ids from breakpoint table")
+                
+                result = conn.execute(text(f"select id from public.\"matches_matches\" WHERE completed_at != 'None';"))
+                df = pd.DataFrame(result.fetchall())
+                
+                match_ids = df['id'].tolist()
+                del df
+                
+                for id in match_ids:
+
+                    header = {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                        "apikey": self.apikey,
+                        "Authorization": "Bearer " + self.apikey
+                    }
+                    
+                    response = requests.get(self.url+str(f"({id})"), headers=header)
+                    self.df = pd.json_normalize(response.json())
+                    
+                    self.check_ids()
+                    self.transform()
+                    self.loader('append')
+
     def breakpoint_matches(self):
         print("Running breakpoint_matches")
         response = requests.get(self.url)
@@ -71,11 +91,13 @@ class CDL_Worker:
             print(f"Failed to retrieve webpage: {response.status_code}")
 
     def check_ids(self):
-        query = f'select * from public."{self.tableName}"'
-        print(query)
+        # query = f'select * from public."{self.tableName}"'
+        # print(query)
         df = pd.read_sql_table(self.tableName, self.engine)
+        # print(df)
         if not df.empty:
             with self.engine.connect() as conn:
+
                 filtered_db_df = self.df[~self.df['id'].isin(df['id'])]
                 print("compared and len is: " + str(len(self.df)))
                 result = conn.execute(text(f'DELETE FROM public."{self.tableName}" WHERE id = ANY (ARRAY[{', '.join(f"'{id}'" for id in filtered_db_df)}]);'))
